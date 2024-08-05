2cc014b57e3822fd9c52b677d5d9141fcc6ded2a
Sergei Runov <runov.sergey@gmail.com>
2024-08-05 13:54:16 +0300

    Initial Commit

---
+++ SparTestApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SparTestApp
+//
+//  Created by Sergei Runov on 05.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SparTestApp/SparTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparTestAppApp.swift
+//  SparTestApp
+//
+//  Created by Sergei Runov on 05.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct SparTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
